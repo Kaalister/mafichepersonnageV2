8484c7c1d6791eb80de2cd0a6ff385b10bd3f4e1
Kaalister <samuel.caillot@epitech.eu>
2020-03-28 18:21:15 +0100

    Updates

---
+++ precache-manifest.ed6fa9a12f4e2c32b92832c61362172e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cf7ffc25c3e464792a8b0e216febcb5",
+    "url": "/mafichepersonnage/index.html"
+  },
+  {
+    "revision": "5bdc6c3d90d88369ce98",
+    "url": "/mafichepersonnage/static/css/main.7b4704db.chunk.css"
+  },
+  {
+    "revision": "0759525e19a4dd540c72",
+    "url": "/mafichepersonnage/static/js/2.c278cebc.chunk.js"
+  },
+  {
+    "revision": "172379c5a4d49ceb9dd9ced5b83600a6",
+    "url": "/mafichepersonnage/static/js/2.c278cebc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5bdc6c3d90d88369ce98",
+    "url": "/mafichepersonnage/static/js/main.8815de54.chunk.js"
+  },
+  {
+    "revision": "1e221007b3828a4dd30f",
+    "url": "/mafichepersonnage/static/js/runtime-main.f3cc1403.js"
+  },
+  {
+    "revision": "283e56f97527181054c23112f47ec74a",
+    "url": "/mafichepersonnage/static/media/avatar.283e56f9.jpg"
+  },
+  {
+    "revision": "08b60efc9b0e1dd6d3fe3fac742a5645",
+    "url": "/mafichepersonnage/static/media/base.08b60efc.jpg"
+  },
+  {
+    "revision": "7cd75e2e432c1180314f7a61aa014d9b",
+    "url": "/mafichepersonnage/static/media/mid.7cd75e2e.jpg"
+  },
+  {
+    "revision": "17d52e9b45f15f4cc43a1df40864e80b",
+    "url": "/mafichepersonnage/static/media/top.17d52e9b.jpg"
+  }
+]);
