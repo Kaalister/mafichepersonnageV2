108685554396fbf1b1d1f632093102b12c051d8e
Kaalister <samuel.caillot@epitech.eu>
2020-03-28 18:03:30 +0100

    Updates

---
+++ precache-manifest.aa1d66eaa084953eec191147926b4b8d.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a936f41ab58f3e17857b8e7fbe724c1c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ebadc18ce90ae0a4a24c",
+    "url": "/static/css/main.7b4704db.chunk.css"
+  },
+  {
+    "revision": "0759525e19a4dd540c72",
+    "url": "/static/js/2.c278cebc.chunk.js"
+  },
+  {
+    "revision": "172379c5a4d49ceb9dd9ced5b83600a6",
+    "url": "/static/js/2.c278cebc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ebadc18ce90ae0a4a24c",
+    "url": "/static/js/main.e5a29c82.chunk.js"
+  },
+  {
+    "revision": "54e57aa271c31fa21ab2",
+    "url": "/static/js/runtime-main.0f26ebec.js"
+  },
+  {
+    "revision": "283e56f97527181054c23112f47ec74a",
+    "url": "/static/media/avatar.283e56f9.jpg"
+  },
+  {
+    "revision": "08b60efc9b0e1dd6d3fe3fac742a5645",
+    "url": "/static/media/base.08b60efc.jpg"
+  },
+  {
+    "revision": "7cd75e2e432c1180314f7a61aa014d9b",
+    "url": "/static/media/mid.7cd75e2e.jpg"
+  },
+  {
+    "revision": "17d52e9b45f15f4cc43a1df40864e80b",
+    "url": "/static/media/top.17d52e9b.jpg"
+  }
+]);
